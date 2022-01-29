e4c2da4cb7f44cc0c747a5adba91a79b0723cbc1
Andrey-Pc <Andrey-Pc@DESKTOP-2NPM8NO>
2022-01-29 11:42:34 -0600

    se agregaron los primertos controller

---
+++ Shopping/Models/Login.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Shopping.Models
+{
+    public class Login
+    {
+        [Required]
+        [Display(Name = "Usuario:")]
+        public string Usuario { set; get; }
+
+        [Required]
+        [Display(Name = "Contraseña:")]
+        public string password { set; get; }
+    }
+}
